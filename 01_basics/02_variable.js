4eef348ee87f9dc1c9766764fd1827094ee2508d
Anurag Singh <anuragsinghtomar092@gmail.com>
2026-01-17 13:02:22 +0000

    variable

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "anurag@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountstate;
+//accountId = 2
+
+accountEmail = "ast@gamil.com"
+accountPassword = "12121212"
+accountCity = "Mumbai"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
