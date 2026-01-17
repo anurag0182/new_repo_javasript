f3a49c3d1240ef1e58ed9d7732064c997fdd5868
Anurag Singh <anuragsinghtomar092@gmail.com>
2026-01-17 14:26:05 +0000

    convension_opt

---
+++ 01_basics/04_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// *****************************   Operations   *******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(3*3);
+//console.log(4-4);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "Hello "
+let str2 = "Anurag"
+
+let str3 = str1 + str2
+
+console.log(str3);
+console.log(1+ 2 +"3");
