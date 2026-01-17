e0e0539dc5f4dfb30e4a12728d8834129a03639e
Anurag Singh <anuragsinghtomar092@gmail.com>
2026-01-17 13:01:48 +0000

    datatype

---
+++ 01_basics/03_data_type.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert(3 + 5) // we are using node.js not browser
+
+console.log(3*3)
+console.log("Hello world")
+
+let name = "anurag"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+//bigint
+//string =>" "
+//booolean => true/false
+// null => standalone value
+// undefined => -
+// symbol => unique
+
+// object
+
+console.log (typeof state);
